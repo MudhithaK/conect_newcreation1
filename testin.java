c566d1b3d4b0153be85e7dd715e22116549bbd03
MudhithaK <mudhithakalpani08@gmail.com>
2024-11-08 17:40:36 +0530

    Update testin.java

--- testin.java
+++ testin.java
@@ -3,5 +3,6 @@ public class tesin{
 public static void main(String []args){
 
 	System.out.println(" test it...");
+	System.out.println("File is successfully added into remote repository....");
     }
 }
